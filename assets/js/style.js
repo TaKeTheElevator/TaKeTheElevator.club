1ce1e5959a8ecc705c195ae0fd03c9976d25fd35
TaKeTheElevator <50219498+TaKeTheElevator@users.noreply.github.com>
2024-06-24 23:19:18 +0300

    Add files via upload

---
+++ assets/js/style.js
@@ -0,0 +1,13 @@
+
+
+
+
+$(function () {
+    window.setTimeout(function () {
+        $('.preloader').addClass('hide-loader');
+        $('.content').addClass('show-content');
+    }, 1500);
+    window.setTimeout(function () {
+        $('.preloader').addClass('kill-loader');
+    }, 4000);
+});
